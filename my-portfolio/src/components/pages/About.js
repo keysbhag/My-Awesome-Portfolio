4bf61ef480ea65a3ef0fc220ed4f1ec4f37a7ad6
keyshawn bhagwandin <keyshawn.bhag@hotmail.com>
2022-11-17 18:44:37 -0500

    finished About page

--- my-portfolio/src/components/pages/About.js
+++ my-portfolio/src/components/pages/About.js
@@ -1,12 +1,37 @@
 import React from "react";
+import '../../styles/About.css'
+import headshot from '../../images/portfolioAboutpic.jpg'
 
 export default function About() {
   return (
     <div>
-      <h1>About</h1>
-      <p className="d-flex">
-            Keyshawn!
-      </p>
+      <h1 className="text-center p-3">About</h1>
+      <div className="d-flex about-container">
+        <div className="flex-fill headshot">
+          <img src={headshot} alt="Keyshawn" height="500"></img>
+        </div>
+        <div className="flex-fill about-info">
+          My name is Keyshawn Bhagwandin. I am a 25 year old young professional
+          Full Stack developer currently residing in the GTA. I am very
+          passionate about coding and web development. I love developing
+          functional projects, responsive web applications, and practical
+          software’s. Some of the technologies I have become very skilled in
+          include JavaScript, MySQL, and Express.JS. I am dedicated to my craft and work
+          to keep build on my already high level skills in programming and
+          coding languages.
+        </div>
+        <div className="flex-fill">
+          <h3> Technologies: </h3>
+          <ul>
+            <li>HTML, CSS, JavaScript</li>
+            <li>Node.JS</li>
+            <li>Express.JS</li>
+            <li>MySQL</li>
+            <li>MongoDB</li>
+            <li>React.JS</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
