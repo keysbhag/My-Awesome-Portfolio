c4bf2021097bf9c6bfd256986e7889a54dbbe5e7
keyshawn bhagwandin <keyshawn.bhag@hotmail.com>
2022-11-17 00:59:05 -0500

    added more styling

--- my-portfolio/src/components/Project.js
+++ my-portfolio/src/components/Project.js
@@ -1,28 +1,41 @@
 import React from "react";
-import "../styles/Header.css";
+import "../styles/Project.css";
+import findflix from '../images/FindFlix.png';
+import foodcity from '../images/FoodCityGrocery.png';
 
 
 const projects = [
   {
     id: 1,
-    src: "FindFlix.png",
-    name: "FindFlix picture",
+    src: findflix,
+    name: "FindFlix",
+    url: "https://keysbhag.github.io/FindFlix-Project/",
   },
   {
     id: 2,
-    src: "FoodCityGrocery.png",
-    name: "Food City picture",
+    src: foodcity,
+    name: "Food City",
+    url: "https://food-city-grocery.herokuapp.com/",
   },
 ];
 
 
 function Project() {
   return (
-
-    <div className="d-flex align-items-center justify-content-center">
+    <div className="d-flex align-items-center justify-content-around flex-wrap">
       {projects.map((project) => (
-        <div className="" key={project.id}>
-          <img src='../images/FindFlix.png' alt={project.name}></img>
+        <div className="project" key={project.id}>
+          <a href={project.url} target="blank" className="example">
+            <img
+              src={project.src}
+              alt={project.name}
+              width="450"
+              height="400"
+            ></img>
+            <div class="content">
+              <h4 className="text"> {project.name} </h4>
+            </div>
+          </a>
         </div>
       ))}
     </div>
--- my-portfolio/src/components/pages/Portfolio.js
+++ my-portfolio/src/components/pages/Portfolio.js
@@ -4,7 +4,7 @@ import Project from '../Project';
 export default function Portfolio() {
   return (
     <div>
-      <h1>Portfolio</h1>
+      <h1 className="text-center p-3">Portfolio</h1>
       <Project/>
     </div>
   );
